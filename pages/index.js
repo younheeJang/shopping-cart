6187eccd04f02c7deaab257c8faa52dcee9e33ee
younheeJang <edam7635@gmail.com>
2021-04-02 22:44:23 +0900

    Setting: global css and basic directory structure

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>Shopping</title>
         <link rel="icon" href="/LOGO.ico" />
       </Head>
-
+    </>
     
    
-    </div>
+   
   )
 }
