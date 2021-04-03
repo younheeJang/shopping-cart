4685bca918a0fca79ab9cef9476d4b0d1115990e
younheeJang <edam7635@gmail.com>
2021-04-03 10:13:14 +0900

    Add Header: with Css

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,15 @@ export default function Home() {
         <title>Curious Jeager</title>
         <link rel="icon" href="/LOGO.ico" />
       </Head>
+      <header className="'shadow-lg bg-white sticky top-0 h-50 text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+              <a className="mr-5 text-gray-900 cursor-pointer" href="/">Home</a>
+              <a className="mr-5 hover:text-gray-900 cursor-pointer" href="/products">Shop</a>
+              <a className="mr-5 hover:text-gray-900 cursor-pointer" href="/cart">Cart</a>
+          </nav>
+        </div>
+      </header>
     </>
     
    
